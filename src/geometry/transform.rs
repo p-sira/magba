a5cd03b292a4a4e3d4684c3a4cd128545f81bcd9
Sira Pornsiriprasert <code@psira.me>
2025-01-12 01:31:47 +0700

    Impl Transform for SourceCollection

--- src/geometry/transform.rs
+++ src/geometry/transform.rs
@@ -11,8 +11,8 @@ pub trait Transform {
     fn orientation(&self) -> UnitQuaternion<f64>;
     fn set_position(&mut self, position: Point3<f64>);
     fn set_orientation(&mut self, orientation: UnitQuaternion<f64>);
-    fn translate(&mut self, translation: Translation3<f64>);
-    fn rotate(&mut self, rotation: UnitQuaternion<f64>);
+    fn translate(&mut self, translation: &Translation3<f64>);
+    fn rotate(&mut self, rotation: &UnitQuaternion<f64>);
 }
 
 /// Implement shallow Transform for objects with no children
@@ -36,12 +36,12 @@ macro_rules! impl_transform {
                 self.orientation = orientation;
             }
 
-            fn translate(&mut self, translation: Translation3<f64>) {
+            fn translate(&mut self, translation: &Translation3<f64>) {
                 self.position = translation.transform_point(&self.position);
             }
 
-            fn rotate(&mut self, rotation: UnitQuaternion<f64>) {
-                self.orientation = rotation * self.orientation;
+            fn rotate(&mut self, rotation: &UnitQuaternion<f64>) {
+                self.orientation = rotation * &self.orientation;
             }
         }
     };
--- src/source/source.rs
+++ src/source/source.rs
@@ -3,7 +3,7 @@
  * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
  */
 
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Translation3, UnitQuaternion, Vector3};
 
 use crate::geometry::Transform;
 
@@ -14,5 +14,49 @@ pub trait Field {
 
 pub trait Source: Transform + Field {}
 
-#[derive(Debug, Clone, PartialEq, Default)]
-pub struct SourceCollection {}
+pub struct SourceCollection {
+    position: Point3<f64>,
+    orientation: UnitQuaternion<f64>,
+
+    sources: [Box<dyn Source>],
+}
+
+impl Transform for SourceCollection {
+    fn position(&self) -> Point3<f64> {
+        self.position
+    }
+
+    fn orientation(&self) -> UnitQuaternion<f64> {
+        self.orientation
+    }
+
+    fn set_position(&mut self, position: Point3<f64>) {
+        let translation = Translation3::from(position - &self.position);
+        self.sources
+            .iter_mut()
+            .for_each(|source| source.translate(&translation));
+        self.position = position
+    }
+
+    fn set_orientation(&mut self, orientation: UnitQuaternion<f64>) {
+        let rotation = orientation * &self.orientation.inverse();
+        self.sources
+            .iter_mut()
+            .for_each(|source| source.rotate(&rotation));
+        self.orientation = orientation;
+    }
+
+    fn translate(&mut self, translation: &Translation3<f64>) {
+        self.sources
+            .iter_mut()
+            .for_each(|source| source.translate(&translation));
+        self.position = translation.transform_point(&self.position);
+    }
+
+    fn rotate(&mut self, rotation: &UnitQuaternion<f64>) {
+        self.sources
+            .iter_mut()
+            .for_each(|source| source.rotate(rotation));
+        self.orientation = rotation * self.orientation;
+    }
+}
