7eec723c2431b63bee92be39c9039038a868a603
Sira Pornsiriprasert <code@psira.me>
2025-01-24 23:20:08 +0700

    Change function signatures.

--- src/fields/conversion.rs
+++ src/fields/conversion.rs
@@ -11,26 +11,28 @@ use crate::constants::MU0;
 
 /// Convert B-field to H-field.
 #[allow(non_snake_case)]
-pub fn B_to_H(B_vector: &mut Vector3<f64>) {
-    B_vector.scale_mut(MU0)
+pub fn B_to_H(B_vector: &Vector3<f64>) -> Vector3<f64> {
+    B_vector.scale(MU0)
 }
 
 /// Convert vector of B-fields to vector of H-fields.
 #[allow(non_snake_case)]
-pub fn Bs_to_Hs(B_vectors: &mut [Vector3<f64>]) {
+pub fn Bs_to_Hs(B_vectors: &[Vector3<f64>]) -> Vec<Vector3<f64>> {
     B_vectors
         .into_iter()
-        .for_each(|vector| vector.scale_mut(MU0))
+        .map(|vector| vector.scale(MU0))
+        .collect()
 }
 
 /// Convert magnetization (**M**) to polarization (**J**).
-pub fn mag_to_pol(mag_vector: &mut Vector3<f64>) {
-    mag_vector.scale_mut(1.0 / MU0)
+pub fn mag_to_pol(mag_vector: &Vector3<f64>) -> Vector3<f64> {
+    mag_vector.scale(1.0 / MU0)
 }
 
 /// Convert magnetizations (**M**) to polarizations (**J**).
-pub fn mags_to_pols(mag_vectors: &mut [Vector3<f64>]) {
+pub fn mags_to_pols(mag_vectors: &[Vector3<f64>]) -> Vec<Vector3<f64>> {
     mag_vectors
         .into_iter()
-        .for_each(|vector| vector.scale_mut(1.0 / MU0))
+        .map(|vector| vector.scale(1.0 / MU0))
+        .collect()
 }
