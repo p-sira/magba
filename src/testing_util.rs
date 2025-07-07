6b7b5e723471133e0141f5ea9f29b03c476f31a7
Sira Pornsiriprasert <code@psira.me>
2025-07-07 23:14:35 +0700

    TEST: Change error criteria to relative distance for vector. Make the error more informative.

--- src/testing_util.rs
+++ src/testing_util.rs
@@ -10,7 +10,7 @@ use std::path::Path;
 use nalgebra::{DMatrix, Point3, UnitQuaternion, Vector3};
 use nalgebra_sparse::io::load_coo_from_matrix_market_file;
 
-use crate::util::is_close;
+use crate::util::{is_close, relative_error, relative_vec_distance};
 
 pub fn load_matrix(path_str: &str) -> DMatrix<f64> {
     let path = Path::new(path_str);
@@ -39,8 +39,16 @@ pub fn quat_from_rotvec(x: f64, y: f64, z: f64) -> UnitQuaternion<f64> {
 
 pub fn assert_close_vector(vec1: &Vector3<f64>, vec2: &Vector3<f64>, rtol: f64) {
     let mut n_fail: usize = 0;
-    vec1.iter().zip(vec2).for_each(|(a, b)| {
-        if !is_close(*a, *b, rtol) {
+    vec1.iter().zip(vec2).enumerate().for_each(|(n, (&a, &b))| {
+        if !is_close(a, b, rtol) {
+            eprintln!(
+                "Element {} mismatch. actual={}, expected={}, relative={:e}, rtol={:e}",
+                n,
+                a,
+                b,
+                relative_error(a, b),
+                rtol
+            );
             n_fail += 1
         }
     });
@@ -57,17 +65,35 @@ pub fn assert_close_vec_vector(vecs1: &Vec<Vector3<f64>>, vecs2: &Vec<Vector3<f6
     }
 
     let mut n_fail: usize = 0;
-    vecs1.iter().zip(vecs2).for_each(|(vec1, vec2)| {
-        for (a, b) in vec1.iter().zip(vec2) {
-            if !is_close(*a, *b, rtol) {
+    let mut worst_rdist = 0.0;
+    let mut worst_params: (usize, Vector3<f64>, Vector3<f64>, f64) =
+        (0, Vector3::default(), Vector3::default(), 0.0);
+    vecs1
+        .iter()
+        .zip(vecs2)
+        .enumerate()
+        .for_each(|(n, (&vec1, &vec2))| {
+            let rdist = relative_vec_distance(vec1, vec2);
+            if rdist > rtol {
+                if rdist > worst_rdist {
+                    worst_rdist = rdist;
+                    worst_params = (n, vec1, vec2, rdist);
+                }
+                eprintln!(
+                    "Vector {} mismatch. actual={:?}, expected={:?}, rdist={:e}, rtol={:e}.",
+                    n, vec1, vec2, rdist, rtol
+                );
                 n_fail += 1;
-                break;
             }
-        }
-    });
+        });
     if n_fail > 0 {
         let percent_fail = n_fail as f64 / len as f64 * 100.0;
-        panic!("assert_close_vector fails. Difference={percent_fail}% ({n_fail}/{len})")
+        eprintln!("Failed {n_fail}/{len} vectors ({percent_fail}%).");
+        eprintln!(
+            "Worst on vector {}: actual={:?}, expected={:?}, rdist={:e}, rtol={:e}.",
+            worst_params.0, worst_params.1, worst_params.2, worst_params.3, rtol
+        );
+        panic!("assert_close_vec_vector")
     }
 }
 
