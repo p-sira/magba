58120bb06271a0bb392ed668f61edd5a7efc463d
Sira Pornsiriprasert <code@psira.me>
2026-02-09 23:55:27 +0700

    ENH: Move transformation logic to Transform's trait default method

--- src/geometry/transform.rs
+++ src/geometry/transform.rs
@@ -20,12 +20,27 @@ pub trait Transform<T: RealField + Copy> {
     fn set_orientation(&mut self, orientation: UnitQuaternion<T>);
     /// Set the object orientation from scaled axis (rotation vector)
     fn set_orientation_from_scaled_axis(&mut self, scaled_axis: nalgebra::Vector3<T>);
+
     /// Translate the object.
-    fn translate(&mut self, translation: &Translation3<T>);
+    fn translate(&mut self, translation: &Translation3<T>) {
+        let new_pos = translation.transform_point(&self.position());
+        self.set_position(new_pos);
+    }
     /// Rotate the object.
-    fn rotate(&mut self, rotation: &UnitQuaternion<T>);
+    fn rotate(&mut self, rotation: &UnitQuaternion<T>) {
+        let new_rot = rotation * self.orientation();
+        self.set_orientation(new_rot);
+    }
     /// Rotate the object using the anchor point as the center of rotation.
-    fn rotate_anchor(&mut self, rotation: &UnitQuaternion<T>, anchor: &Point3<T>);
+    fn rotate_anchor(&mut self, rotation: &UnitQuaternion<T>, anchor: &Point3<T>) {
+        let local_position = self.position() - anchor;
+        
+        let new_pos = rotation * local_position + anchor.coords;
+        let new_rot = rotation * self.orientation();
+
+        self.set_position(new_pos.into());
+        self.set_orientation(new_rot);
+    }
 }
 
 /// Macro to implement [`Transform`].
@@ -57,34 +72,28 @@ macro_rules! impl_transform {
             self.orientation = nalgebra::UnitQuaternion::from_scaled_axis(scaled_axis);
         }
     };
-    (@shallow_transform) => {
+    (@builders) => {
         #[inline]
-        fn translate(&mut self, translation: &nalgebra::Translation3<T>) {
-            self.position = translation.transform_point(&self.position);
+        pub fn with_position(mut self, position: impl Into<nalgebra::Point3<T>>) -> Self {
+            self.position = position.into();
+            self
         }
 
         #[inline]
-        fn rotate(&mut self, rotation: &nalgebra::UnitQuaternion<T>) {
-            self.orientation = rotation * self.orientation;
+        pub fn with_orientation(mut self, orientation: impl Into<nalgebra::UnitQuaternion<T>>) -> Self {
+            self.orientation = orientation.into();
+            self
         }
 
         #[inline]
-        fn rotate_anchor(
-            &mut self,
-            rotation: &nalgebra::UnitQuaternion<T>,
-            anchor: &nalgebra::Point3<T>,
-        ) {
-            let local_position = self.position - anchor;
-            self.position = nalgebra::Point3::from(
-                rotation * local_position + nalgebra::Vector3::from(anchor.coords),
-            );
-            self.orientation = rotation * self.orientation;
+        pub fn with_orientation_from_scaled_axis(mut self, scaled_axis: impl Into<nalgebra::Vector3<T>>) -> Self {
+            self.orientation = nalgebra::UnitQuaternion::from_scaled_axis(scaled_axis.into());
+            self
         }
     };
     () => {
         crate::geometry::impl_transform!(@getters);
         crate::geometry::impl_transform!(@setters);
-        crate::geometry::impl_transform!(@shallow_transform);
     };
 }
 pub(crate) use impl_transform;
