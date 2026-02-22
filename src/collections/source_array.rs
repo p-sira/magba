20b3e990df1bc0c305c1fa60823af1d3e09b73ee
Sira Pornsiriprasert <code@psira.me>
2026-02-23 03:02:30 +0700

    ENH: Use impl_group_transform

--- src/collections/source_array.rs
+++ src/collections/source_array.rs
@@ -3,11 +3,14 @@
  * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
  */
 
+use std::usize;
+
 use nalgebra::{Point3, Translation3, UnitQuaternion, Vector3};
 
 use crate::{
     base::{Field, Float, Source, Transform, transform::impl_transform},
     geometry::Pose,
+    transform::impl_group_transform,
 };
 
 #[derive(Debug, Clone)]
@@ -42,55 +45,9 @@ impl<S: Source<T>, T: Float, const N: usize> SourceArray<S, T, N> {
             offsets,
         }
     }
-
-    pub fn set_pose(&mut self, new_pose: impl Into<Pose<T>>) {
-        self.pose = new_pose.into();
-
-        // Re-calculate absolute positions for all children
-        // This wipes out any drift that might have occurred
-        for (child, offset) in self.children.iter_mut().zip(&self.offsets) {
-            let global_isometry = self.pose.as_isometry() * offset.as_isometry();
-            child.set_pose(global_isometry.into());
-        }
-    }
-
-    delegate::delegate! {
-        to self.pose {
-            pub fn position(&self) -> Point3<T>;
-            pub fn orientation(&self) -> UnitQuaternion<T>;
-        }
-    }
-
-    pub fn set_position(&mut self, position: impl Into<Translation3<T>>) {
-        let mut new_pose = *self.pose();
-        new_pose.set_position(position);
-        self.set_pose(new_pose);
-    }
-
-    pub fn set_orientation(&mut self, orientation: UnitQuaternion<T>) {
-        let mut new_pose = *self.pose();
-        new_pose.set_orientation(orientation);
-        self.set_pose(new_pose);
-    }
-
-    pub fn translate(&mut self, translation: impl Into<Translation3<T>>) {
-        let mut new_pose = *self.pose();
-        new_pose.translate(translation);
-        self.set_pose(new_pose);
-    }
-
-    pub fn rotate(&mut self, rotation: UnitQuaternion<T>) {
-        let mut new_pose = *self.pose();
-        new_pose.rotate(rotation);
-        self.set_pose(new_pose);
-    }
-
-    pub fn rotate_anchor(&mut self, rotation: UnitQuaternion<T>, anchor: impl Into<Point3<T>>) {
-        let mut new_pose = *self.pose();
-        new_pose.rotate_anchor(rotation, anchor);
-        self.set_pose(new_pose);
-    }
 }
+
+impl_group_transform!(SourceArray<S, T, N> where S: Source<T>, T: Float, const N: usize);
 
 impl<S: Source<T> + Clone, T: Float, const N: usize> Source<T> for SourceArray<S, T, N> {}
 
