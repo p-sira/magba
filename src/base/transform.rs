1591a970fdf50fd732c4e4a282837b7e2f6e1bd1
Sira Pornsiriprasert <code@psira.me>
2026-02-22 15:32:18 +0700

    ENH: Create reusable macro for implementing group transform

--- src/base/transform.rs
+++ src/base/transform.rs
@@ -38,3 +38,55 @@ macro_rules! impl_transform {
     };
 }
 pub(crate) use impl_transform;
+
+macro_rules! impl_group_transform {
+    ($name:ident < $( $args:ident ),* > where $( $bounds:tt )* ) => {
+        impl< $( $bounds )* > $name< $( $args ),*> {
+            pub fn set_pose(&mut self, new_pose: impl Into<Pose<T>>) {
+                self.pose = new_pose.into();
+                for (child, offset) in self.children.iter_mut().zip(&self.offsets) {
+                    let global_isometry = self.pose.as_isometry() * offset.as_isometry();
+                    child.set_pose(global_isometry.into());
+                }
+            }
+
+            delegate::delegate! {
+                to self.pose {
+                    pub fn position(&self) -> Point3<T>;
+                    pub fn orientation(&self) -> UnitQuaternion<T>;
+                }
+            }
+
+            pub fn set_position(&mut self, position: impl Into<Translation3<T>>) {
+                let mut new_pose = *self.pose();
+                new_pose.set_position(position);
+                self.set_pose(new_pose);
+            }
+
+            pub fn set_orientation(&mut self, orientation: UnitQuaternion<T>) {
+                let mut new_pose = *self.pose();
+                new_pose.set_orientation(orientation);
+                self.set_pose(new_pose);
+            }
+
+            pub fn translate(&mut self, translation: impl Into<Translation3<T>>) {
+                let mut new_pose = *self.pose();
+                new_pose.translate(translation);
+                self.set_pose(new_pose);
+            }
+
+            pub fn rotate(&mut self, rotation: UnitQuaternion<T>) {
+                let mut new_pose = *self.pose();
+                new_pose.rotate(rotation);
+                self.set_pose(new_pose);
+            }
+
+            pub fn rotate_anchor(&mut self, rotation: UnitQuaternion<T>, anchor: impl Into<Point3<T>>) {
+                let mut new_pose = *self.pose();
+                new_pose.rotate_anchor(rotation, anchor);
+                self.set_pose(new_pose);
+            }
+        }
+    };
+}
+pub(crate) use impl_group_transform;
