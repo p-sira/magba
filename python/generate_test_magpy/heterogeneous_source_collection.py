aed0980cdf2fca504681252d3aae3a3e0bc33980
Sira Pornsiriprasert <code@psira.me>
2026-02-23 16:14:15 +0700

    TEST: Rename python files for generating tests

--- python/generate_test_magpy/heterogeneous_source_collection.py
+++ python/generate_test_magpy/heterogeneous_source_collection.py
@@ -4,6 +4,7 @@
 import sys
 from pathlib import Path
 
+from magpylib.misc import Dipole
 import numpy as np
 from magpylib import Collection
 from magpylib.magnet import Cuboid, Cylinder
@@ -13,9 +14,11 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from test_generation_util import get_points_small, save_test_array
 
+
 def test_multi_collection(points):
     rot1 = Rotation.identity()
     rot2 = Rotation.from_rotvec([0, np.pi / 3, 0])
+    rot3 = Rotation.from_rotvec([0, np.pi / 2, np.pi / 2])
 
     magnets = Collection(
         Cylinder(
@@ -30,6 +33,7 @@ def test_multi_collection(points):
             (0.02, 0.02, 0.03),
             (0.1, 0.2, 0.3),
         ),
+        Dipole((0, 0, 0), rot3, (0.4, 0.5, 0.6)),
     )
 
     field = magnets.getB(points)
