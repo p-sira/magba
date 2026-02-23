ccacc014f7a66ac588a7949689c2c7b4ac63e9b5
Sira Pornsiriprasert <code@psira.me>
2026-02-23 22:53:20 +0700

    DOC: Format document

--- src/fields/field_zero.rs
+++ src/fields/field_zero.rs
@@ -5,12 +5,17 @@
 
 use nalgebra::{Point3, RealField, UnitQuaternion, Vector3};
 
+/// Return zero vectors for all points.
 pub fn zero_field<T: RealField>(
     points: &[Point3<T>],
     _: &Point3<T>,
     _: &UnitQuaternion<T>,
     out: &mut [Vector3<T>],
 ) {
-    assert_eq!(out.len(), points.len(), "Output slice length must match points length.");
+    assert_eq!(
+        out.len(),
+        points.len(),
+        "Output slice length must match points length."
+    );
     out.fill(Vector3::zeros());
 }
