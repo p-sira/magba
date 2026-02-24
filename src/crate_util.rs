47f6821d88964f8e8f4550f320f5948a20b4387f
Sira Pornsiriprasert <code@psira.me>
2026-02-24 20:21:08 +0700

    FMT

--- src/crate_util.rs
+++ src/crate_util.rs
@@ -15,6 +15,18 @@ macro_rules! need_std {
 }
 pub(crate) use need_std;
 
+macro_rules! pub_on_feature {
+    {$feature:literal, $($kw:ident {$($item:ident),+ $(,)?})+ $(,)?} => {
+        $($(
+            #[cfg(feature=$feature)]
+            pub $kw $item;
+            #[cfg(not(feature=$feature))]
+            pub(crate) $kw $item;
+        )+)+
+    };
+}
+pub(crate) use pub_on_feature;
+
 macro_rules! format_float {
     ($v: expr) => {
         $v.to_string()
@@ -98,15 +110,3 @@ macro_rules! impl_parallel_sum {
     }};
 }
 pub(crate) use impl_parallel_sum;
-
-macro_rules! pub_on_feature {
-    {$feature:literal, $($kw:ident {$($item:ident),+ $(,)?})+ $(,)?} => {
-        $($(
-            #[cfg(feature=$feature)]
-            pub $kw $item;
-            #[cfg(not(feature=$feature))]
-            pub(crate) $kw $item;
-        )+)+
-    };
-}
-pub(crate) use pub_on_feature;
