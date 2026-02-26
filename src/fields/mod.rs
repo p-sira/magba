3aa122f8025134e7e59082cfc974ec5e9cd8a501
Sira Pornsiriprasert <code@psira.me>
2026-02-26 21:08:04 +0700

    CHG: Remove ZeroMagnet

--- src/fields/mod.rs
+++ src/fields/mod.rs
@@ -9,9 +9,6 @@ pub(crate) mod field_cuboid;
 pub(crate) mod field_cylinder;
 pub(crate) mod field_dipole;
 
-mod field_zero;
-
 pub use field_cuboid::{cuboid_B, cuboid_B_batch, sum_multiple_cuboid_B};
 pub use field_cylinder::{cylinder_B, cylinder_B_batch, sum_multiple_cylinder_B};
 pub use field_dipole::{dipole_B, dipole_B_batch, sum_multiple_dipole_B};
-pub use field_zero::{zero_field, zero_field_batch};
