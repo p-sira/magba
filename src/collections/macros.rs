3d5b8e679e343a984f153a37d92b2d6fac194a80
Sira Pornsiriprasert <code@psira.me>
2026-03-03 11:44:35 +0700

    DOC: Add docs for sources macro

--- src/collections/macros.rs
+++ src/collections/macros.rs
@@ -3,6 +3,23 @@
  * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
  */
 
+/// Constructs a source composite.
+///
+/// # Examples
+///
+/// ```
+/// # use magba::*;
+/// let cylinder = CylinderMagnet::default();
+/// let cuboid = CuboidMagnet::default();
+/// let dipole = Dipole::default();
+/// let dipole2 = Dipole::default().with_moment([1.0, 0.0, 0.0]);
+///
+/// let assembly: SourceAssembly = sources!(cylinder, cuboid, dipole);
+///
+/// let homogenous_array: SourceArray<Dipole, _> = sources!([dipole, dipole2]);
+/// let heterogenous_array: SourceArray<Magnet, _> = 
+///     sources!([cylinder.into(), cuboid.into(), dipole.into()]);
+/// ```
 #[macro_export]
 macro_rules! sources {
     // sources!([magnet1, magnet2, ...])
