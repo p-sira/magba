c2b12aba3ef8f284607eed403d8413609e0e7b5d
Sira Pornsiriprasert <code@psira.me>
2026-03-05 19:08:13 +0700

    DOC: Add doc for using f32

--- src/base/math.rs
+++ src/base/math.rs
@@ -7,7 +7,36 @@ const MU0: f64 = 1.2566370614359173e-6;
 
 /// Generic trait for floating point numbers compatible with all [Magba](crate) implementations.
 ///
-/// Supports [f32] and [f64].
+/// Supports [f32] and [f64]. Magba defaults to `f64`. You can explicitly use `f32` for improved
+/// performance at the cost of precision.
+///
+/// # Examples
+///
+/// Constants: Permeability of free space (μ₀)
+/// ```
+/// # use magba::base::Float;
+/// assert_eq!(f64::mu0(), 1.2566370614359173e-6);
+/// assert_eq!(f32::mu0(), 1.256637e-6);
+///
+/// assert_ne!(f64::mu0(), f32::mu0() as f64);
+/// ```
+///
+/// Using in magnetic field calculations
+/// ```
+/// # use magba::prelude::*;
+/// # use nalgebra::vector;
+/// # use approx::{assert_relative_eq, assert_relative_ne};
+/// let cylinder_f64 = CylinderMagnet::<f64>::default();
+/// let cylinder_f32 = CylinderMagnet::<f32>::default();
+///
+/// let field_f64 = cylinder_f64.compute_B([0.0, 0.0, 0.5].into());
+/// let field_f32 = cylinder_f32.compute_B([0.0, 0.0, 0.5].into());
+///
+/// assert_relative_eq!(field_f64, vector![0.0, 0.0, 0.4472135954999579]);
+/// assert_relative_eq!(field_f32, vector![0.0, 0.0, 0.4472136]);
+///
+/// assert_relative_ne!(field_f64, field_f32.cast::<f64>());
+/// ```
 pub trait Float: nalgebra::RealField + num_traits::Float + Copy {
     /// Permeability of free space (μ₀) = 4π × 10⁻⁷ H/m.
     fn mu0() -> Self;
