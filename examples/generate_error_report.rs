5ba48659c80b575522ece7d637cbdb5daff48351
Sira Pornsiriprasert <code@psira.me>
2026-05-06 10:49:30 +0700

    DOC: Add accuracy report

--- examples/generate_error_report.rs
+++ examples/generate_error_report.rs
@@ -6,12 +6,47 @@
 use magba::base::mesh::TriMesh;
 use magba::prelude::*;
 use magba::testing_util::*;
-use nalgebra::{Point3, Vector3, vector};
-use reproducible::ReportBuilder;
+use nalgebra::{Point3, vector};
+use reproducible::metrics;
+use reproducible::prelude::*;
 use std::error::Error;
 use std::f64::consts::PI;
 
-fn create_magnet<T: magba::base::Float + std::str::FromStr>(name: &str) -> Box<dyn Source<T>>
+/// Extension trait to convert a `Source` into a closure compatible with `reproducible`'s batch API
+trait SourceExt {
+    fn as_batch_evaluator(self) -> Box<dyn Fn(&[Vec<f64>]) -> Vec<Vec<f64>> + Send + Sync>;
+}
+
+impl<T: magba::base::Float + std::str::FromStr> SourceExt for Box<dyn Source<T> + Send + Sync> {
+    fn as_batch_evaluator(self) -> Box<dyn Fn(&[Vec<f64>]) -> Vec<Vec<f64>> + Send + Sync> {
+        Box::new(move |inputs: &[Vec<f64>]| {
+            let points: Vec<Point3<T>> = inputs
+                .iter()
+                .map(|i| {
+                    Point3::new(
+                        T::from(i[0]).unwrap(),
+                        T::from(i[1]).unwrap(),
+                        T::from(i[2]).unwrap(),
+                    )
+                })
+                .collect();
+            self.compute_B_batch(&points)
+                .into_iter()
+                .map(|v| {
+                    vec![
+                        v.x.to_f64().unwrap(),
+                        v.y.to_f64().unwrap(),
+                        v.z.to_f64().unwrap(),
+                    ]
+                })
+                .collect()
+        })
+    }
+}
+
+fn create_magnet<T: magba::base::Float + std::str::FromStr>(
+    name: &str,
+) -> Box<dyn Source<T> + Send + Sync>
 where
     CylinderMagnet<T>: Source<T>,
     CuboidMagnet<T>: Source<T>,
@@ -28,22 +63,21 @@ where
     let rot = quat_from_rotvec(f(PI / 7.0), f(PI / 6.0), f(PI / 5.0));
     let pol = [f(1.0), f(2.0), f(3.0)];
 
+    let tetrahedron_vertices = [
+        vector![f(-0.1), f(-0.1), f(-0.1)],
+        vector![f(0.1), f(-0.1), f(-0.1)],
+        vector![f(0.0), f(0.1), f(-0.1)],
+        vector![f(0.0), f(0.0), f(0.1)],
+    ];
+
     match name {
         "CylinderMagnet" => Box::new(CylinderMagnet::new(pos, rot, pol, f(0.1), f(0.2))),
         "CuboidMagnet" => Box::new(CuboidMagnet::new(pos, rot, pol, [f(0.1), f(0.2), f(0.3)])),
         "Dipole" => Box::new(Dipole::new(pos, rot, pol)),
         "SphereMagnet" => Box::new(SphereMagnet::new(pos, rot, pol, f(0.1))),
-        "TetrahedronMagnet" => Box::new(TetrahedronMagnet::new(
-            pos,
-            rot,
-            pol,
-            [
-                vector![f(-0.1), f(-0.1), f(-0.1)],
-                vector![f(0.1), f(-0.1), f(-0.1)],
-                vector![f(0.0), f(0.1), f(-0.1)],
-                vector![f(0.0), f(0.0), f(0.1)],
-            ],
-        )),
+        "TetrahedronMagnet" => {
+            Box::new(TetrahedronMagnet::new(pos, rot, pol, tetrahedron_vertices))
+        }
         "TriangleMagnet" => Box::new(TriangleMagnet::new(
             pos,
             rot,
@@ -56,12 +90,7 @@ where
         )),
         "MeshMagnet" => {
             let mesh = TriMesh::<T>::new_unchecked(
-                vec![
-                    Vector3::new(f(-0.1), f(-0.1), f(-0.1)),
-                    Vector3::new(f(0.1), f(-0.1), f(-0.1)),
-                    Vector3::new(f(0.0), f(0.1), f(-0.1)),
-                    Vector3::new(f(0.0), f(0.0), f(0.1)),
-                ],
+                tetrahedron_vertices,
                 vec![[0, 2, 1], [0, 1, 3], [1, 2, 3], [0, 3, 2]],
             );
             Box::new(MeshMagnet::new(pos, rot, pol, mesh))
@@ -70,12 +99,12 @@ where
     }
 }
 
-fn add_magnet_accuracy<T: magba::base::Float + std::str::FromStr, S: Source<T> + ?Sized>(
-    builder: ReportBuilder,
+fn add_magnet_accuracy<T: magba::base::Float + std::str::FromStr>(
+    report: Report,
     name: &str,
-    source: &S,
+    source: Box<dyn Source<T> + Send + Sync>,
     ref_file: &str,
-) -> Result<ReportBuilder, Box<dyn Error>> {
+) -> Result<Report, Box<dyn Error>> {
     let points_path = std::path::Path::new("./tests/test-data/points.csv");
     let ref_path = std::path::Path::new("./tests/test-data/").join(ref_file);
 
@@ -83,48 +112,10 @@ fn add_magnet_accuracy<T: magba::base::Float + std::str::FromStr, S: Source<T> +
         return Err(format!("Test data not found for {}", name).into());
     }
 
-    let expected = matrix_to_vector_vec(&load_matrix_from_csv::<f64>(&ref_path));
-    let points_raw = load_matrix_from_csv::<f64>(&points_path);
-
-    let points_t: Vec<Point3<T>> = points_raw
-        .row_iter()
-        .map(|row| {
-            Point3::new(
-                T::from(row[0]).unwrap(),
-                T::from(row[1]).unwrap(),
-                T::from(row[2]).unwrap(),
-            )
-        })
-        .collect();
-
-    let b_fields_t = source.compute_B_batch(&points_t);
-
-    let errors: Vec<f64> = b_fields_t
-        .iter()
-        .zip(expected.iter())
-        .map(|(a, b)| {
-            let actual_f64 = Vector3::new(
-                a.x.to_f64().unwrap(),
-                a.y.to_f64().unwrap(),
-                a.z.to_f64().unwrap(),
-            );
-
-            let diff = (actual_f64 - b).norm();
-            let norm_b = b.norm();
-
-            if norm_b > 1e-12 {
-                diff / norm_b
-            } else {
-                diff
-            }
-        })
-        .collect();
-
-    let stats = reproducible::Stats::from_samples(&errors);
-    Ok(builder.add_accuracy_row(reproducible::AccuracyReportRow {
-        name: name.to_owned(),
-        stats,
-    }))
+    Ok(report.with_row(
+        Row::new_batch(name, source.as_batch_evaluator())
+            .with_split_csv_cases(points_path, ref_path)?,
+    ))
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -140,32 +131,38 @@ fn main() -> Result<(), Box<dyn Error>> {
         ("MeshMagnet", "triangularmesh.csv"),
     ];
 
+    fn get_report() -> Report {
+        Report::new()
+            .with_float_precision(3)
+            .with_metric(metrics::rel_err)
+            .with_column(Column::accuracy("Median").with_stat(ColumnStat::Median))
+            .with_column(Column::accuracy("Mean").with_stat(ColumnStat::Mean))
+            .with_column(Column::accuracy("P95").with_stat(ColumnStat::P95))
+            .with_column(Column::accuracy("Max").with_stat(ColumnStat::Max))
+    }
+
     // f64 report
     {
-        println!("## Precision: f64 (Relative Error)\n");
-        let mut builder = ReportBuilder::new();
-        builder.render_config_mut().error_unit = "".to_owned();
-        builder.render_config_mut().float_precision = 3;
+        println!("## Relative Error: f64\n");
+        let mut report = get_report();
 
         for &(name, ref_file) in &magnets {
             let magnet = create_magnet::<f64>(name);
-            builder = add_magnet_accuracy(builder, name, &*magnet, ref_file)?;
+            report = add_magnet_accuracy(report, name, magnet, ref_file)?;
         }
-        println!("{}", builder.render_accuracy_markdown());
+        println!("{}", report.render_markdown());
     }
 
     // f32 report
     {
-        println!("\n## Precision: f32 (Relative Error)\n");
-        let mut builder = ReportBuilder::new();
-        builder.render_config_mut().error_unit = "".to_owned();
-        builder.render_config_mut().float_precision = 3;
+        println!("\n## Relative Error: f32\n");
+        let mut report = get_report();
 
         for &(name, ref_file) in &magnets {
             let magnet = create_magnet::<f32>(name);
-            builder = add_magnet_accuracy(builder, name, &*magnet, ref_file)?;
+            report = add_magnet_accuracy(report, name, magnet, ref_file)?;
         }
-        println!("{}", builder.render_accuracy_markdown());
+        println!("{}", report.render_markdown());
     }
 
     Ok(())
